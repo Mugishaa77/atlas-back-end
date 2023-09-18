7608f3ee69a0cf42c35b9f8fac6b9a0d2b3773b1
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-18 13:30:18 +0300

    recreated backend server

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Atlas');
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://sallywanga2016:Mugisha77@cluster0.2atceva.mongodb.net/atlasTeaBrokersLimited', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+db.once('open', () => {
+  console.log('Connected to Mugisha-s Database');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
