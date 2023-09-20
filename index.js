f24747e49f0ab2a08315d06ee67450c0215e4f04
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-20 15:35:30 +0300

    moved the market reports to the root folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path'); // Import the path module
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,12 +14,17 @@ app.get('/', (req, res) => {
 app.use(cors());
 app.use(bodyParser.json());
 
+// Serve static PDF files from the "reports" folder with proper content type
+app.use('/reports', (req, res, next) => {
+  // Set the Content-Type header to indicate that the response is a PDF
+  res.setHeader('Content-Type', 'application/pdf');
+  next();
+}, express.static(path.join(__dirname, 'reports')));
+
 mongoose.connect('mongodb+srv://sallywanga2016:Mugisha77@cluster0.2atceva.mongodb.net/atlasTeaBrokersLimited', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-
 
 const db = mongoose.connection;
 db.on('error', (error) => {
