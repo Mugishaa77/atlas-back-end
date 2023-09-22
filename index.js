624ac9ae84b56033b3da7df92e16990dc117fba7
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-22 13:56:41 +0300

    added excel files to the server

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const bodyParser = require('body-parser');
 const path = require('path'); // Import the path module
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to Atlas');
 });
+
+app.get('/test', (req, res) => {
+  res.send('This is a test route');
+});
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,8 +23,18 @@ app.use(bodyParser.json());
 app.use('/reports', (req, res, next) => {
   // Set the Content-Type header to indicate that the response is a PDF
   res.setHeader('Content-Type', 'application/pdf');
+  console.log('Request received for PDF:', req.url); // Add this line for debugging
   next();
 }, express.static(path.join(__dirname, 'reports')));
+
+
+app.use('/excel', (req, res, next) => {
+  console.log('Request received for excel:', req.url);
+  // Set the Content-Type header to indicate that the response is an Excel file
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  next();
+}, express.static(path.join(__dirname, 'excel')));
+
 
 mongoose.connect('mongodb+srv://sallywanga2016:Mugisha77@cluster0.2atceva.mongodb.net/atlasTeaBrokersLimited', {
   useNewUrlParser: true,
@@ -36,4 +51,10 @@ db.once('open', () => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
 });
